e3a161001972f0205fa1cdaac2f00273576041f0
amitanshusahu <amitanshusahu.official@gmail.com>
2022-10-31 20:56:14 +0530

    feat: updation of menu content dynamically

---
+++ js/menuData.js
@@ -0,0 +1,62 @@
+const menuData = [
+    {
+        language:[
+            {
+                chapter:"Values Tokens and Operators",
+                topic:[
+                    "Valuse",
+                    "Numbers",
+                    "Operators",
+                    "Valuse"
+                ],
+            },
+            {
+                chapter:"Chapter 2",
+                topic:[
+                    "Valuse",
+                    "Numbers",
+                    "Operators",
+                ],
+            },
+        ],
+        browser:[
+            {
+                chapter:"Values Tokens and Operators",
+                topic:[
+                    "Valuse",
+                    "Numbers",
+                    "Operators",
+                    "Valuse"
+                ],
+            },
+            {
+                chapter:"Chapter 2",
+                topic:[
+                    "Valuse",
+                    "Numbers",
+                    "Operators",
+                ],
+            },
+        ],
+        beyond:[
+            {
+                chapter:"Values Tokens and Operators",
+                topic:[
+                    "Valuse",
+                    "Numbers",
+                    "Operators",
+                    "Valuse"
+                ],
+            },
+            {
+                chapter:"Chapter 2",
+                topic:[
+                    "Valuse",
+                    "Numbers",
+                    "Operators",
+                ],
+            },
+        ],
+      
+    }
+]
